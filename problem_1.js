a1f36f821707b959cd135bb1792b0c6136eb9907
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-01-15 22:08:00 +0530

    update problem 1 - unsolved

--- problem_1.js
+++ problem_1.js
@@ -1,10 +1,26 @@
 /*
 Problem Name - Longest Consecutive Sequence:
-Problem Statement - 
-Write a function that takes an array of integers and 
+
+Problem Statement - Write a function that takes an array of integers and 
 returns the length of the longest consecutive sequence of numbers.
 
 Input: [100, 4, 200, 1, 3, 2]
 
 Output: 4
 */
+
+function longestConsecutiveSequence(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+var arr = [204, 201, 100, 200, 1, 3, 202, 2, 203];
+
+console.log(longestConsecutiveSequence(arr));
